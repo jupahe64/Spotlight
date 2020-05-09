848ad271eab4a6676d8b8895d50be218f90b5869
jupahe64 <33004544+jupahe64@users.noreply.github.com>
2020-05-09 18:47:37 +0200

    fix ZoneTransforms hopefully for the final time

--- SpotLight/EditorDrawables/SM3DWorldScene.cs
+++ SpotLight/EditorDrawables/SM3DWorldScene.cs
@@ -520,8 +520,6 @@ namespace SpotLight.EditorDrawables
 
                     EditZoneTransform = mainZone.ZonePlacements[value - 1].GetTransform();
 
-                    SceneDrawState.ZoneTransform = EditZoneTransform;
-
                     for (int i = 0; i < mainZone.ZonePlacements.Count; i++)
                     {
                         if (i == value - 1)
@@ -533,6 +531,8 @@ namespace SpotLight.EditorDrawables
 
                     ZonePlacements.Add(new ZonePlacement(Vector3.Zero, Vector3.Zero, Vector3.One, mainZone));
                 }
+
+                SceneDrawState.ZoneTransform = EditZoneTransform;
 
                 undoStack = EditZone.undoStack;
                 redoStack = EditZone.redoStack;
